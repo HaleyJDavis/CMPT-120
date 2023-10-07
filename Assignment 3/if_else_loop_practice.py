ef0941b6c0b91702a5e3f846d3f44f02b8ca5e0e
HaleyJDavis <haley.davis115@outlook.com>
2023-10-07 11:22:47 -0400

    Finished 3

--- Assignment 3/if_else_loop_practice.py
+++ Assignment 3/if_else_loop_practice.py
@@ -1,7 +1,7 @@
 #first of all, breathe
 #don't let this intimidate you. break it down a part at a time. You've done all of these separately and now we're just combining them.
 '''
-Instructions: Create a list with at least 8 numbers, and make sure you have a raandom dispersement between 0-50
+Instructions: Create a list with at least 8 numbers, and make sure you have a random dispersement between 0-50
 Create a loop (which loop would be best given we know how long we're going [the length of the list]?) that runs through the entire list
 Inside of your loop, have an if/elif/else that checks AT EACH INDEX OF THE LIST:
 if the number is greater than 35: print ("greater than 35")
@@ -11,6 +11,15 @@ else (the number is less than 5)
 '''
 
 def main():
-    
+    numList = ["33", "18", "23", "27", "10", "3", "7", "31", "48", "39"]
+    for x in numList:
+        if int(x) > 35:
+            print(int(x), "is greater than 35")
+        elif int(x) > 20 and int(x) < 35:
+            print(int(x), "is between 20-35")
+        elif int(x) > 5 and int(x) < 20:
+            print(int(x), "is between 5-20")
+        else:
+            print(int(x), "is less than 5")
     
 main()
